67e0718dff4e67b71fcb77747741cfa8fa52e368
SMoore3773 <Smooresculpture@gmail.com>
2020-04-14 21:27:41 -0400

    dependancies installed

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <section>all the stuff here</section>
+    </div>
+  );
+}
+
+export default App;
